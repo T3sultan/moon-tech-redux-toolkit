de1e48d493fee7d66df39bf5570623aa83fd2e4e
unknown <{T3sultan}@users.noreply.github.com>
2023-01-16 16:48:42 +0600

    update

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
+import logger from "redux-logger";
 import cartSlice from "../features/cart/cartSlice";
 import filterSlice from "../features/filter/filterSlice";
+import productsSlice from "../features/products/productsSlice";
 
 export const store = configureStore({
-    // devTools:false,
+  // devTools:false,
   reducer: {
     cart: cartSlice,
     filter: filterSlice,
+    products:productsSlice
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -12,7 +12,7 @@ const cartSlice = createSlice({
       const selectedProduct = state.cart.find(
         (product) => product._id === action.payload._id
       );
-      console.log(action);
+      //   console.log(action);
       if (!selectedProduct) {
         const product = {
           ...action.payload,
@@ -26,8 +26,24 @@ const cartSlice = createSlice({
           .push(selectedProduct);
       }
     },
+    removeFromCart: (state, action) => {
+      if (action.payload.quantity > 1) {
+        const product = {
+          ...action.payload,
+          quantity: action.payload.quantity - 1,
+        };
+        state.cart = state.cart.filter(
+          (product) => product._id !== action.payload._id
+        );
+        state.cart.push(product);
+      } else {
+        state.cart = state.cart.filter(
+          (product) => product._id !== action.payload._id
+        );
+      }
+    },
   },
 });
-export const { addToCart } = cartSlice.actions;
+export const { addToCart,removeFromCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
